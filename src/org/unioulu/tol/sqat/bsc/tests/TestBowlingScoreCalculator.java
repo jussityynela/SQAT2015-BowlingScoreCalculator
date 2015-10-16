d0c0687b63ad0daf0d9f6a6443821e586ca9e68a
somename <someemail>
2015-10-16 11:59:58 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -12,17 +12,16 @@ public class TestBowlingScoreCalculator {
 	public void testGetScoreAtTheBeginningOfTheGameZero() {
 	//
 	BowlingGame bowlingGame = new BowlingGame();	
-	 assertEquals(0,bowlingGame.score());
-	}
+	 	assertEquals(0,bowlingGame.score());
+	}	
 	
 	@Test
 	public void testPlayOneFrameOfGameSeeThatFrameIsAdded() {
 	//
-	BowlingGame bowlingGame = new BowlingGame();
-	Frame frame = new Frame(10,0);
-	bowlingGame.addFrame(frame) ;
-	assertEquals(10,bowlingGame.score());
-	 
+		BowlingGame bowlingGame = new BowlingGame();
+		Frame frame = new Frame(10,0);
+		bowlingGame.addFrame(frame) ;
+		assertEquals(10,bowlingGame.score());	 
 	}
 
 }
