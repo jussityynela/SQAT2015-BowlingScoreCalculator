062044637d1f99f22366014dcc9049d7cad05062
somename <someemail>
2015-10-16 11:46:53 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -3,12 +3,17 @@ package org.unioulu.tol.sqat.bsc.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat.bsc.BowlingGame;
 
 public class TestBowlingScoreCalculator {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetScoreAtTheBeginningOfTheGame() {
+		//
+	BowlingGame bowlingGame = new BowlingGame();
+		
+		
+	 assertEquals()
 	}
 
 }
