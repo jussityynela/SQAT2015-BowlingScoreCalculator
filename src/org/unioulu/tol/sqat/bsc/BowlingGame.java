df6aab6ee4a9a5d320c509ed39e1af6e43cd6c3a
somename <someemail>
2015-10-16 12:30:37 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/BowlingGame.java
+++ src/org/unioulu/tol/sqat/bsc/BowlingGame.java
@@ -26,6 +26,12 @@ public class BowlingGame {
 		for(int i=0; i < frames.size(); i++)
 		{
 			score += frames.get(i).score();
+			//if previous was spare add bonus
+			if(i>0)
+			{
+				Frame previous = frames.get(i-1);
+				if(frames.get(i-1).)
+			}
 		}
 		return score;
 	}
