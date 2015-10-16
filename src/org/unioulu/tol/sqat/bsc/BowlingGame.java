f1d1dfc9450f412632bff625abb9a05edad6a614
Jussi Tyynelä <jussi.tyynela@gmail.com>
2015-10-16 13:42:25 +0300

    Project finished.

--- src/org/unioulu/tol/sqat/bsc/BowlingGame.java
+++ src/org/unioulu/tol/sqat/bsc/BowlingGame.java
@@ -3,6 +3,8 @@ package org.unioulu.tol.sqat.bsc;
 import java.util.ArrayList;
 import java.util.List;
 
+
+//finished at 13:41
 public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
