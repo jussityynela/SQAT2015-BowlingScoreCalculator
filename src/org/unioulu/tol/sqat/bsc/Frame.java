324e718b4f959578924065400a100056b5915029
somename <someemail>
2015-10-16 12:11:01 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/Frame.java
+++ src/org/unioulu/tol/sqat/bsc/Frame.java
@@ -23,7 +23,8 @@ public class Frame {
 	//return whether a frame is a spare or not
 	public boolean isSpare(){
 		//to be implemented
-		return false;
+		if( firstThrow < 10 && score() == 10)
+			return true;
 	}
 
 	//return whether this is the last frame of the match
